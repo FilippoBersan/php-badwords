abc636ebf90ae928bb18ef212ccd8fe0a2f0ac46
FilippoBersan <filippobersan99@gmail.com>
2024-01-08 14:46:19 +0100

    feat: primo file con form

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Badwords</title>
+</head>
+<body>
+
+<form action="dati.php" method="GET">
+  <label for="frase">Frase:</label>
+  <input type="text" id="frase">
+  <label for="parola">Parola da censurare:</label>
+  <input type="text" id="parola">
+  <button>Invia il modulo</button>
+  
+
+
+</form>
+  
+</body>
+</html>
